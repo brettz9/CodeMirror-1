40691d281b92214fd08ebdc6627e96e828556d48
Brett Zamir <brettz9@yahoo.com>
2018-10-02 11:05:25 +0800

    - Enhancement: Add ES6 Module distributed file to build process; add minified files with terser and add source map files - Enhancement: For ESM bundlers, point to new distributed ESM entrance file as `module` in `package.json` - Refactoring: Switch from Buble to Babel 7 with preset-env (Buble was breaking on array destructuring) and update to latest Rollup API - npm: Update node-static, Rollup deps; add npm-recommended `package-lock.json` and `package.json` linting recommended meta-data fields

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,31 @@
-import buble from 'rollup-plugin-buble';
+import babel from 'rollup-plugin-babel';
+import {terser} from 'rollup-plugin-terser';
 
-export default {
-  banner: `// CodeMirror, copyright (c) by Marijn Haverbeke and others
-// Distributed under an MIT license: https://codemirror.net/LICENSE
+function getBundleInfo ({format, file, min = false}) {
+  return {
+    input: "src/codemirror.js",
+    output: {
+      format,
+      file,
+      name: "CodeMirror",
+      sourcemap: true,
+      banner: `// CodeMirror, copyright (c) by Marijn Haverbeke and others
+  // Distributed under an MIT license: https://codemirror.net/LICENSE
 
-// This is CodeMirror (https://codemirror.net), a code editor
-// implemented in JavaScript on top of the browser's DOM.
-//
-// You can find some technical background for some of the code below
-// at http://marijnhaverbeke.nl/blog/#cm-internals .
-`,
-  entry: "src/codemirror.js",
-  format: "umd",
-  dest: "lib/codemirror.js",
-  moduleName: "CodeMirror",
-  plugins: [ buble({namedFunctionExpressions: false}) ]
-};
+  // This is CodeMirror (https://codemirror.net), a code editor
+  // implemented in JavaScript on top of the browser's DOM.
+  //
+  // You can find some technical background for some of the code below
+  // at http://marijnhaverbeke.nl/blog/#cm-internals .
+  `
+    },
+    plugins: [ babel(), min ? terser() : null ]
+  };
+}
+
+export default [
+  getBundleInfo({format: 'es', file: "dist/codemirror-es.js"}),
+  getBundleInfo({format: 'es', file: "dist/codemirror-es.min.js", min: true}),
+  getBundleInfo({format: 'umd', file: "lib/codemirror.js"}),
+  getBundleInfo({format: 'umd', file: "lib/codemirror.min.js", min: true})
+];
